e0d16a568f2c886a5b202a4cb58459b726f8b619
Mohammad Broughani <gitmbroughani81@gmail.com>
2024-10-17 21:59:25 +0330

    ~

--- src/flow/pages/core.cljs
+++ src/flow/pages/core.cljs
@@ -3,8 +3,7 @@
    [flow.subs :as subs]
 
    [re-frame.core :as rf]
-   [markdown.core :as md]
-   ))
+   [markdown.core :as md]))
 
 (defn render-markdown [markdown-content]
   (let [html-content (md/md-to-html-string markdown-content)]
@@ -12,8 +11,14 @@
 
 (defn about-page [content]
   ;; [:p content]
-  [render-markdown content]
-  )
+  ;; [render-markdown content]
+  [:div {:style {:display "flex" :justify-content "space-between" :align-items "flex-start"}}
+   [:div ;; Content on the left
+    [render-markdown content]]
+   [:img {:src   "images/profile.jpg"
+          :alt   "Description of the image"
+          :style {:max-width   "200px"  ;; Adjust the size as needed
+                  :margin-left "20px"}}]])
 
 (defn cv-page [content]
   [:p content])
