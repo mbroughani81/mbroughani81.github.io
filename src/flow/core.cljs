34ecfc9f3ca10ba410dfa098af8e58ff46b1ea7d
Mohammad Broughani <gitmbroughani81@gmail.com>
2025-04-08 16:39:10 +0330

    ~

--- src/flow/core.cljs
+++ src/flow/core.cljs
@@ -38,7 +38,7 @@
     [:div.header-right
      [:ul
       [:li {:class "header-item"} [:a {:href "/about"} "about"]]
-      [:li {:class "header-item"} [:a {:href "https://drive.google.com/file/d/1tjEuijHQIGnxZhwvgr4RY61Zai-8lO4o/view?usp=sharing"} "cv"]]
+      [:li {:class "header-item"} [:a {:href "https://drive.google.com/file/d/1ivFpiV837uX1g7HoKde2PPAZu-bgA6MH/view"} "cv"]]
       ;; [:li {:class "header-item"} [:a {:href "/repos"} "repos"]]
       ]]]])
 
